eb50ea0b1bf8328799eb543348a4c03ec81a00dc
Mustakim <mustakimcode@gmail.com>
2022-11-23 14:50:46 +0700

    add email reminder for subscription almost ended

--- app/Console/Commands/SendDueRemider.php
+++ app/Console/Commands/SendDueRemider.php
@@ -5,14 +5,20 @@ namespace App\Console\Commands;
 use Illuminate\Console\Command;
 use App\Models\UserPackage;
 
-class SendDueEmailRemider extends Command
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+
+class SendDueRemider extends Command
 {
     /**
      * The name and signature of the console command.
      *
      * @var string
      */
-    protected $signature = 'package:email:reminder:due';
+    protected $signature = 'package:due:reminder';
 
     /**
      * The console command description.
@@ -29,9 +35,16 @@ class SendDueEmailRemider extends Command
     public function handle()
     {
         $userPackages = UserPackage::where('expired_date', '<', date('Y-m-d', strtotime('+7 days')))
-            ->get();
+            ->with('user')->get();
         foreach ($userPackages as $key => $value) {
-            echo $value->expired_date . "\r\n";
+            //should send email and sms notification here
+            $details = [
+                'title' => 'Your Subscription About to ended',
+                'body' => 'Hi, your subscription will end at '.date("Y-m-d", strtotime($value->expired_date))
+            ];
+
+            \Mail::to($value->user->email)->send(new \App\Mail\DueEMail($details));
+          
         }
         return Command::SUCCESS;
     }
